326432b9a9294ce482e989aa246c6ddd36cf1c5c
pik <pik30072@gmail.com>
2016-07-31 10:59:10 +0700

    intet to webview

--- app/src/main/java/com/datasofthosting/administrator/mytraffic/MainActivity.java
+++ app/src/main/java/com/datasofthosting/administrator/mytraffic/MainActivity.java
@@ -1,14 +1,32 @@
 package com.datasofthosting.administrator.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit ประกาศตัวแปร
+    private String urlYoutubeString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //Main method
+
+    public void clickAboutMe(View view) {
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.effect_btn_long);
+        mediaPlayer.start();
+
+        //Intent to WebView การเคลื่อนย้ายการทำงาน
+        urlYoutubeString = "https://youtu.be/wMgDHi4Eq_c";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlYoutubeString));
+        startActivity(intent);
     }
-}
+}   //Main class
